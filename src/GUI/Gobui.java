19646d83d917d4ae2ac4c8a5dbbde008681b642f
dominique-molee <dxm6029@rit.edu>
2018-04-14 16:37:35 -0400

    no gabi

--- src/GUI/Gobui.java
+++ src/GUI/Gobui.java
@@ -29,8 +29,37 @@ public class Gobui extends GridPane{
         Button insurance = new Button("Insurance");
         Button legal = new Button("Legal");
         Button management = new Button("Management");
-        Button accounting = new Button("Accounting");
-        Button accounting = new Button("Accounting");
+        Button manufacturing = new Button("Manufacturing");
+        Button marketing = new Button("Marketing");
+        Button math = new Button("Math");
+        Button med = new Button("Medical");//dental, info, nurse, tech
+        Button nursing = new Button("Nursing Supervisor");
+        Button personal = new Button("Personal");
+        Button pharmacy = new Button("Pharmacy");
+        Button productSpecialist = new Button("Product Specialist");
+        Button project = new Button("Project");
+        Button protective = new Button("Protective");
+        Button realEstate = new Button("Real Estate");
+        Button retail = new Button("Retail");
+        Button sales = new Button("Sales");//sales rep and just sales
+        Button sanitation = new Button("Sanitation");
+        Button science = new Button("Science");
+        Button service = new Button("Service");
+        Button socialScience = new Button("Social Science");
+        Button speechLangPath = new Button("Speech Language Pathologist");
+        Button sports = new Button("Sports");
+        Button steward = new Button("Steward");
+        Button tech = new Button("Tech"); //tech, help, info, software
+        Button therapy = new Button("Therapy");
+        Button transport = new Button("Transport");
+        Button vet = new Button("Vet");
+        Button warehouse = new Button("Warehouse");
+
+        this.getChildren().addAll(accounting, admin, arch, arts, aviation, care, construction, customer, driver,
+                education, eng, facilitator, finance, food, hospitality, hr, install, insurance, legal,
+                management, manufacturing, marketing, med, math, nursing, personal, pharmacy, productSpecialist,
+                project, protective, realEstate, retail, sales, sanitation, science, service, socialScience,
+                speechLangPath, sports, steward, tech, therapy, transport, vet, warehouse);
 
 
     }
