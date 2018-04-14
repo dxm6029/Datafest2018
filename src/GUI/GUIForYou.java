09f3ef6465959f47db7fe765d6841197f9e462e7
dominique-molee <dxm6029@rit.edu>
2018-04-14 15:12:01 -0400

    no gui

--- src/GUI/GUIForYou.java
+++ src/GUI/GUIForYou.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -14,70 +15,83 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class GUIForYou extends Application{
 
-    public HBox buttons(){
-        HBox buttons = new HBox();
-        Button states = new Button();
-        VBox cities = new VBox();
-        VBox state = new VBox();
-        Button city = new Button("CITIES");
-        Label lState = new Label("STATES");
-        Label lCity = new Label("CITIES");
-
-        //states formatting
-        states.setMaxSize(225, 200);
-        states.setMinSize(225, 200);
-        //city formatting
-        city.setMaxSize(225, 200);
-        city.setMinSize(225, 200);
-        //setting city image
-        Image iCity = new Image(getClass().getResourceAsStream("cities-nyc_optimized.jpg"));
-        ImageView imageCity = new ImageView(iCity);
-        imageCity.setFitHeight(200);
-        imageCity.setFitWidth(205);
-        city.setGraphic(imageCity);
-        //setting state image
-        Image iState = new Image(getClass().getResourceAsStream("61POqINKAbL._SX425_.jpg"));
-        ImageView imageState = new ImageView(iState);
-        imageState.setFitHeight(200);
-        imageState.setFitWidth(205);
-        states.setGraphic(imageState);
-
-        cities.getChildren().addAll(city,lCity);
-        cities.setAlignment(Pos.CENTER);
-        state.getChildren().addAll(states, lState);
-        state.setAlignment(Pos.CENTER);
-
-        buttons.getChildren().add(state);
-        buttons.getChildren().add(cities);
-        return buttons;
-    }
-
-    public VBox getThatData(){
-        VBox dataview = new VBox();
-        Label l = new Label("Out Here Learning");
-        l.setStyle("");
-        dataview.getChildren().add(l);
-        dataview.setAlignment(Pos.CENTER);
-        return dataview;
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("DataFest Graphs");
-        VBox mainView = new VBox();
-        HBox buttons =  buttons();
-        buttons.setMaxSize(450, 250);
-        buttons.setMinSize(450, 250);
-        VBox dataView = getThatData();
-
-        mainView.getChildren().add(buttons);
-        mainView.getChildren().add(dataView);
-        Scene graphDisplay = new Scene(mainView);
-        stage.setScene(graphDisplay);
-        stage.show();
-    }
+//    public HBox buttons(){
+//        HBox buttons = new HBox();
+//        Button states = new Button();
+//        VBox cities = new VBox();
+//        VBox state = new VBox();
+//        Button city = new Button("CITIES");
+//        Label lState = new Label("STATES");
+//        Label lCity = new Label("CITIES");
+//
+//        //making city and state label pretty
+//        lCity.setStyle("-fx-font-size: 20px; -fx-font-weight: bold; -fx-text-fill: blue;");
+//        lState.setStyle("-fx-font-size: 20px; -fx-font-weight: bold; -fx-text-fill: blue;");
+//
+//        //states formatting
+//        states.setMaxSize(223, 200);
+//        states.setMinSize(223, 200);
+//        states.setStyle("-fx-background-color: lightblue");
+//
+//        //city formatting
+//        city.setMaxSize(223, 200);
+//        city.setMinSize(223, 200);
+//        city.setStyle("-fx-background-color: lightblue");
+//
+//        //setting city image
+//        Image iCity = new Image(getClass().getResourceAsStream("cities-nyc_optimized.jpg"));
+//        ImageView imageCity = new ImageView(iCity);
+//        imageCity.setFitHeight(200);
+//        imageCity.setFitWidth(205);
+//        city.setGraphic(imageCity);
+//
+//        //setting state image
+//        Image iState = new Image(getClass().getResourceAsStream("61POqINKAbL._SX425_.jpg"));
+//        ImageView imageState = new ImageView(iState);
+//        imageState.setFitHeight(200);
+//        imageState.setFitWidth(205);
+//        states.setGraphic(imageState);
+//
+//        // adding stuff to vBoxes
+//        cities.getChildren().addAll(city,lCity);
+//        cities.setAlignment(Pos.CENTER);
+//        state.getChildren().addAll(states, lState);
+//        state.setAlignment(Pos.CENTER);
+//
+//        //adding stuff to final hBox
+//        buttons.getChildren().add(state);
+//        buttons.setSpacing(8);
+//        buttons.getChildren().add(cities);
+//        return buttons;
+//    }
+//
+//    public VBox getThatData(){
+//        VBox dataview = new VBox();
+//        Label l = new Label("Out Here Learning");
+//        l.setStyle("-fx-font-size: 50px; -fx-font-weight: bold; -fx-text-fill: blue; -fx-effect: innershadow(three-pass-box , rgba(0,0,0,0.7) , 6, 0.0 , 0 , 2); ");
+//        dataview.getChildren().add(l);
+//        dataview.setAlignment(Pos.CENTER);
+//        return dataview;
+//    }
+//
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//        stage.setTitle("DataFest Graphs");
+//        VBox mainView = new VBox();
+//        HBox buttons =  buttons();
+//        buttons.setMaxSize(456, 250);
+//        buttons.setMinSize(456, 250);
+//        VBox dataView = getThatData();
+//
+//        mainView.getChildren().add(buttons);
+//        mainView.getChildren().add(dataView);
+//        Scene graphDisplay = new Scene(mainView);
+//        stage.setScene(graphDisplay);
+//        stage.show();
+//    }
 }
